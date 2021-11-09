dbd746ef72371d929176331981d3babffc0b34a7
heerreeraa <jonherrera03@gmail.com>
2021-11-09 10:12:03 +0100

    front productos, archivos back productos & listas

--- public/js/frontproductos.js
+++ public/js/frontproductos.js
@@ -1,58 +1,98 @@
-// Get the modal
-var modal = document.querySelector("#myModal");
+// END FUNCIONAMIENTO DEL MODAL
+    // Get the modal
+    var modal = document.querySelector("#myModal");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
     modal.style.display = "none";
-  }
-};
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    };
+    document
+    .querySelector("#btn-agregar-producto")
+    .addEventListener("click", function (event) {
+        event.preventDefault();
+        modal.style.display = "block";
+    });
+// END FUNCIONAMIENTO DEL MODAL
+
+// INSERTAR EN LOCAL -->
 document
-  .querySelector("#btn-agregar-producto")
+  .querySelector("#btn-insertar-producto")
   .addEventListener("click", function (event) {
     event.preventDefault();
-    modal.style.display = "block";
+    alert("Producto insertado correctamente pá!");
+
+    let nombre = document.querySelector("input[name='nombre-producto']").value;
+    let precio = document.querySelector("input[name='precio-producto']").value;
+
+    let producto = {
+      nombre: nombre,
+      precio: precio
+    };
+    console.log(producto);
+    let body = JSON.stringify(producto);
+
+    fetch("/productos/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: body,
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (res) {
+        console.log(res);
+      });
   });
-// // INSERTAR EN LOCAL -->
-// document
-//   .querySelector("#btn-insertar")
-//   .addEventListener("click", function (event) {
-//     event.preventDefault();
-//     alert("Insertado pa");
+// <-- END INSERTAR EN LOCAL
 
-//     let nombre = document.querySelector("input[name='nombre']").value;
-//     let apellido = document.querySelector("input[name='apellido']").value;
-//     let dni = document.querySelector("input[name='dni']").value;
-//     let tel = document.querySelector("input[name='tel']").value;
 
-//     let cliente = {
-//       nombre: nombre,
-//       apellido: apellido,
-//       dni: dni,
-//       tel: tel,
-//     };
-//     console.log(cliente);
-//     let body = JSON.stringify(cliente);
 
-//     fetch("/clientes/", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: body,
-//     })
-//       .then(function (response) {
-//         return response.json();
-//       })
-//       .then(function (res) {
-//         console.log(res);
-//       });
-//   });
+
+// GenerateHTML(productos) {
+//     productos.forEach((producto) => {
+//       let item = document.createElement("div");
+//       item.className = "producto";
+//       item.id = `producto${num}`;
+//       let pro = document.createElement("producto");
+
+//       pro.src = `${producto.producto_files[0].link} muted`;
+
+//       let divpro = document.createElement("div");
+//       divpro.id = `divpro${num}`;
+
+//       let divBtn = document.createElement("div");
+//       divBtn.id = `caja${num}`;
+
+//       let btnFav = document.createElement("button");
+//       btnFav.id = `btn${num}`;
+//       btnFav.className = `btnClass`;
+
+//       let imgFav = document.createElement("img");
+//       imgFav.id = `${producto.producto_files[0].link}`;
+//       imgFav.src = `imgs/star.png`;
+//       imgFav.className = `false${num}`;
+//       document.querySelector("#producto-container").appendChild(item);
+
+//       // document.querySelector(`#producto${num}`).appendChild(divpro);
+//       // document.querySelector(`#producto${num}`).appendChild(divBtn);
+
+//       document.querySelector(`#producto${num}`).appendChild(btnFav);
+//       document.querySelector(`#btn${num}`).appendChild(imgFav);
+//       document.querySelector(`#producto${num}`).appendChild(pro);
+
+//       num++;
+//     });
+   
+//   }
