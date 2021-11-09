cb2ff8f81e431f87a78a21dad1d201c35bc4ff89
L3G3ND <rapitxu@hotmail.com>
2021-11-09 10:40:29 +0100

    listar clientes ready

--- public/js/frontclientes.js
+++ public/js/frontclientes.js
@@ -58,3 +58,31 @@ document
         console.log(res);
       });
   });
+function cargarClientes() {
+  fetch("/clientes/", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (res) {
+      console.log(res);
+
+      res.forEach((photo) => {
+        const item = document.createElement("div");
+        item.id = `${photo.DNI}`;
+        item.classList.add("item");
+
+        item.innerHTML = `
+          <a target="_blank">
+            <img class="img-zoom" src="./styles/user.jpg">
+          </a>
+          `;
+        document.querySelector(".gallery").appendChild(item);
+      });
+    });
+}
+cargarClientes();
