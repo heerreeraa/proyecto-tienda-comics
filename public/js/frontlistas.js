9fa6f97f3430a4199d2cd07329b54c0d05e4acaa
L3G3ND <rapitxu@hotmail.com>
2021-11-10 13:18:40 +0100

    arreglao

--- public/js/frontlistas.js
+++ public/js/frontlistas.js
@@ -1,24 +1,23 @@
-function cargarClientesSelect() {
-    fetch("/listas/", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
+function cargarClientes() {
+  fetch("/listas/cargarClientes", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then(function (response) {
+      return response.json();
     })
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (res) {
-        console.log(res);
-  
-        res.forEach((sel) => {
-          const item = document.createElement("option");
-          item.id = `${sel.Nombre}`;
-          item.classList.add("option-cliente");
-  
-          item.innerHTML = `${sel.Nombre}`;
-          document.querySelector(".select-usuario-ventas").appendChild(item);
-        });
-        generarModalClient();
+    .then(function (res) {
+      console.log(res);
+
+      res.forEach((photo) => {
+        const item = document.createElement("option");
+        item.id = `${photo.Nombre}`;
+
+        item.innerHTML = `${photo.Nombre}`;
+        document.querySelector(".select-usuario-ventas").appendChild(item);
       });
-  }
+    });
+}
+cargarClientes();
--- routes/backlistas.js
+++ routes/backlistas.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", function (request, response) {
-    let db = request.app.locals.db;
-    db.collection("clientes")
-      .find()
-      .toArray(function (err, datos) {
-        if (err != undefined) {
-          console.log(err);
-          response.send({ mensaje: "error: " + err });
-        } else {
-          console.log(datos);
-          response.send(datos);
-        }
-      });
-  });
+router.get("/cargarClientes", function (request, response) {
+  let db = request.app.locals.db;
+  db.collection("clientes")
+    .find()
+    .toArray(function (err, datos) {
+      if (err != undefined) {
+        console.log(err);
+        response.send({ mensaje: "error: " + err });
+      } else {
+        console.log(datos);
+        response.send(datos);
+      }
+    });
+});
 
 module.exports = router;
